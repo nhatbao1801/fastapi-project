864fe679186ef58055290154e69d0eaf6fcaecb2
phuocVV2 <phuocVV2@fpt.com>
2023-12-12 13:28:55 +0700

    [phuocvv] init module

---
+++ src/config/system.py
@@ -0,0 +1,9 @@
+class SystemConfig:
+    """System configuration class"""
+
+    PROJECT_NAME: str = "GPT"
+    DESCRIPTION: str = "Alitaa"
+    VERSION: str = "0.0.1"
+
+
+cfg_system = SystemConfig()
